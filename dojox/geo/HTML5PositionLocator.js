0a3dcd5d31fbd927124802396910b2c68b05fe8a
José Moreira <MatrixOwnsYou@gmail.com>
2010-08-09 18:51:00 +0100

    improved html5 geo location code

--- dojox/geo/HTML5PositionLocator.js
+++ dojox/geo/HTML5PositionLocator.js
@@ -9,7 +9,8 @@ dojo.declare("dojox.geo.HTML5PositionLocator", null, {
   constructor: function() {
   },
 
-  //
+  // Features
+  //TODO: maybe use 'features' list like dojox.data for the available position attributes?
   isCoordenateSupportAvailable: function() {
     return navigator!=="undefined" && navigator.geolocation!=="undefined";
   },
@@ -24,10 +25,30 @@ dojo.declare("dojox.geo.HTML5PositionLocator", null, {
     return FALSE;
   },
 
-  //
+  // Wrapps a call to HTML5 'navigator.geolocation.getCurrentPosition'
   getCurrentPosition: function(callback, errBack, options) {
-    navigator.geolocation.getCurrentPosition(callback, errBack, options);
+    // var cb = dojo.hitch(this, '_getCurrentPositionCallback');
+    // var eb = dojo.hitch(this, '_getCurrentPositionErrback');
+    // navigator.geolocation.getCurrentPosition(cb, eb, options);
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new dojox.geo.Position(position.coords.latitude, position.coords.longitude);
+      callback(pos);
+      }, errBack, options);
+
   },
+
+  // Wrapper for the real call, to pass Coordinate/Position data types to the user
+  _getCurrentPositionCallback: function(position) {
+    
+  },
+
+  // Wrapper for the real errback
+  _getCurrentPositionErrback: function(err) {
+    //TODO do something usefull
+    console.error(err);
+  },
+
+
 
   //
   watchPosition: function(callback, errBack, options) {
